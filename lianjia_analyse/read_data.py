b9b37f4aea4f18b9959fdbdaa2229d40bac5ba4e
zjx424 <41108631+zjx424@users.noreply.github.com>
2019-03-08 12:01:12 +0800

    Update read_data.py

--- lianjia_analyse/read_data.py
+++ lianjia_analyse/read_data.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 def get_df():
     table_name = input('input tabel_name:')
     '''想办法将账户密码IP地址隐藏'''
-    engine = create_engine('mysql+pymysql://root:456765xx@120.77.39.227:3306/lianjia')
+    engine = create_engine("passwd_and_user")
 
     sq='select * from %s'%table_name
     sql=sq+';'
